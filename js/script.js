58af44e5ce00928bf23477f8f569a237a2131502
CAIO AGUIAR <caio.athanasio.de.sousa.aguiar@tecnico.ulisboa.pt>
2025-08-25 13:18:57 +0100

    Melhorando a semântica e estilos na seção de perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ parametros.forEach((produto) => {
   const param = document.getElementById(produto);
   if (param) param.checked = true;
 });
+
+// Faq perguntas
+const perguntas = document.querySelectorAll(".perguntas button");
+
+/** Modifica o ´aria-expanded´ da tag <dt>, e a visibilidade do <dd> respectivo
+ *  de acordo com o valor presente no atributo ´aria-controls´ do <dt>
+ *
+ * @param {Event} event
+ */
+function handleClickFaq(event) {
+  const texto = document.getElementById(
+    event.target.getAttribute("aria-controls")
+  );
+  event.target.setAttribute(
+    "aria-expanded",
+    texto.classList.toggle("faq-ativo")
+  );
+}
+
+perguntas.forEach((pergunta) => {
+  pergunta.addEventListener("click", handleClickFaq);
+});
