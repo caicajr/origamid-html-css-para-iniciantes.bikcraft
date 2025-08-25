a89882d527bb2d84980937fbe0fc63b122edc8a7
CAIO AGUIAR <caio.athanasio.de.sousa.aguiar@tecnico.ulisboa.pt>
2025-08-25 13:44:50 +0100

    Troca de imagens da galeria

--- js/script.js
+++ js/script.js
@@ -28,3 +28,41 @@ function handleClickFaq(event) {
 perguntas.forEach((pergunta) => {
   pergunta.addEventListener("click", handleClickFaq);
 });
+
+// Galeria de bicicletas
+/**
+ * @param {Event} event
+ * @param {Element} img
+ * @param {Number} minWidth
+ *  */
+function imagemTroca(img, atributo, minWidth, event) {
+  if (!matchMedia(`(min-width: ${minWidth})`).matches) return;
+
+  let temp = img.getAttribute(atributo);
+  img.setAttribute(atributo, event.target.getAttribute(atributo));
+  event.target.setAttribute(atributo, temp);
+}
+
+/** Para cada atributo de cada elemento, adiciona um evento de 'click',
+ *  que chama a função 'imagemTroca'
+ *
+ * @param {Array} galeria
+ * @param {Element} mainImg
+ * @param {Number} minWidth
+ * */
+function galeriaTroca(galeria, mainImg, minWidth) {
+  galeria.forEach((elemento) => {
+    Array.from(elemento.attributes).forEach((atributo) => {
+      elemento.addEventListener(
+        "click",
+        imagemTroca.bind(null, mainImg, atributo.name, minWidth)
+      );
+    });
+  });
+}
+
+const galeria = Array.from(document.querySelectorAll(".bicicleta-imagens img"));
+const mainImg = galeria.shift();
+const minWidth = "800px";
+
+if (galeria.length && mainImg) galeriaTroca(galeria, mainImg, minWidth);
